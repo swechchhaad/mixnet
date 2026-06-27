1a5c347d2758b846132125707ec1ed775de53d00
Swechchha Adhikari <swechchha.adhikari4@gmail.com>
2026-06-27 04:10:46 +0200

    update parameters to match the paper

--- include/params.h
+++ include/params.h
@@ -1,10 +1,10 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
-#define N 16        // ring dimension (tiny for learning)
-#define Q 257       // modulus
+#define N 512        // ring dimension
+#define Q 3109       // modulus
 #define K 4        // seed length
 #define P 8
-#define ETA 1   // noise bound
+#define ETA 2   // noise bound
 
 #endif
